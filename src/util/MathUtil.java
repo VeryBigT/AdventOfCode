c69f9a98294fe90b0c7b317ad76c520e3d878e37
Tobias Rogalski <dante.rogalski@gmail.com>
2025-12-25 13:47:20 +0100

    - added min and max to MathUtil

--- src/util/MathUtil.java
+++ src/util/MathUtil.java
@@ -11,6 +11,46 @@ public abstract class MathUtil {
         return result >= 0 ? result : result + b;
     }
 
+    public static int min(int... ints) {
+        if (ints.length == 0)
+            throw new IllegalArgumentException("Input array is empty");
+        int result = ints[0];
+        for (int i = 1; i < ints.length; i++)
+            if (ints[i] < result)
+                result = ints[i];
+        return result;
+    }
+
+    public static long min(long... ints) {
+        if (ints.length == 0)
+            throw new IllegalArgumentException("Input array is empty");
+        long result = ints[0];
+        for (int i = 1; i < ints.length; i++)
+            if (ints[i] < result)
+                result = ints[i];
+        return result;
+    }
+
+    public static int max(int... ints) {
+        if (ints.length == 0)
+            throw new IllegalArgumentException("Input array is empty");
+        int result = ints[0];
+        for (int i = 1; i < ints.length; i++)
+            if (ints[i] > result)
+                result = ints[i];
+        return result;
+    }
+
+    public static long max(long... ints) {
+        if (ints.length == 0)
+            throw new IllegalArgumentException("Input array is empty");
+        long result = ints[0];
+        for (int i = 1; i < ints.length; i++)
+            if (ints[i] > result)
+                result = ints[i];
+        return result;
+    }
+
     public static void main(String[] args) {
     }
 }
